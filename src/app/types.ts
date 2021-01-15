6a25cd929bae74415f0eada2eb41a28e6d46b761
Pix3l Click3r <blancaflorkevin@gmail.com>
2021-01-15 05:59:13 +0100

    commit on main

---
+++ src/app/types.ts
@@ -0,0 +1,22 @@
+export interface Poll extends PollForm {
+  id: number, //12
+  results: number[]; // [0, 0, 0, 0, 5, 7, 2]
+  voted: boolean;
+}
+
+export interface PollForm {
+  question: string; // Which days of the week do you like the most?
+  options: string[];  // ["Monday", "Tuesday", "Wednesday" ...]
+  thumbnail: string; // https://image.png
+}
+
+export interface PollVote {
+  id: number;
+  vote: number;
+}
+
+export interface Voter {
+  id: string; // 0xJHSADJH5412SXD
+  voted: number[]; // [12]
+
+}
